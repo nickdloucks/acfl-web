005a843aa3fb9db1ce301e82d8fd16a60a6f3b7f
Nick <ndloucks@proton.me>
2025-10-30 20:24:42 -0700

    teams can adjust mosre traits year to year

--- entities/team.go
+++ entities/team.go
@@ -3,7 +3,6 @@ package entities
 type Team struct {
 	// note: the Team object transcends "Year".
 	// Player stats, team stats, and game results are assigned to a year
-	Id         string `json:"id"`   // primary key, uuid
-	Abrv       string `json:"abrv"`       // unique, abbreviation for the team name
-	Name       string `json:"name"` // unique
+	Id   string `json:"id"`   // primary key, uuid
+	Name string `json:"name"` // unique
 }
--- entities/team_year.go
+++ entities/team_year.go
@@ -1,17 +1,14 @@
 package entities
 
 type TeamYear struct {
-	Id         string `json:"id"` // primary key
+	Id         string `json:"id"`   // primary key
+	Name       string `json:"name"` // unique
 	Year       uint16 `json:"year"`
-	Name       string `json:"name"` // unique, format: YYYY-Team Name
+	Abrv       string `json:"abrv"` // unique, abbreviation for the team name
 	City       string `json:"home_city"`
 	Conference string `json:"conference"` // foreign key
 	Color1     string `json:"color_1"`    // hex color code
 	Color2     string `json:"color_2"`    // hex color code
 	Color3     string `json:"color_3"`    // hex color code
 	Color4     string `json:"color_4"`    // hex color code
-	// Roster   string `json:"roster"`   // list of PlayerYear objects
-	// Schedule string `json:"schedule"` // list of MatchResult objects
-	// Stats    string `json:"stats"`    // list of TeamMatchStats objects
-	// note: team stats for the whole year should be dynamically calulated so that they are auto-updating
 }
