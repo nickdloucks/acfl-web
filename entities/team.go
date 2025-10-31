bd4c40e127432fb9f38865fb393c95b661b6f36e
Nick <ndloucks@proton.me>
2025-10-30 20:24:02 -0700

    allow teams possibility to adjust color scheme, comference, city, abrv from year to year

--- entities/team.go
+++ entities/team.go
@@ -4,15 +4,6 @@ type Team struct {
 	// note: the Team object transcends "Year".
 	// Player stats, team stats, and game results are assigned to a year
 	Id         string `json:"id"`   // primary key, uuid
-	Name       string `json:"name"` // unique
-	City       string `json:"home_city"`
 	Abrv       string `json:"abrv"`       // unique, abbreviation for the team name
-	Conference string `json:"conference"` // foreign key
-	Color1     string `json:"color_1"`    // hex color code
-	Color2     string `json:"color_2"`    // hex color code
-	Color3     string `json:"color_3"`    // hex color code
-	Color4     string `json:"color_4"`    // hex color code
-	// Logo1Path  string `json:"logo_1_path"` 
-	// Logo2Path  string `json:"logo_2_path"`
-	// probably don't need these ^ paths, can just have the ui construct the path using some sort of naming convention
+	Name       string `json:"name"` // unique
 }
--- entities/team_year.go
+++ entities/team_year.go
@@ -1,9 +1,15 @@
 package entities
 
 type TeamYear struct {
-	Id   string `json:"id"` // primary key
-	Year uint16 `json:"year"`
-	Name string `json:"name"` // unique, format: YYYY-Team Name
+	Id         string `json:"id"` // primary key
+	Year       uint16 `json:"year"`
+	Name       string `json:"name"` // unique, format: YYYY-Team Name
+	City       string `json:"home_city"`
+	Conference string `json:"conference"` // foreign key
+	Color1     string `json:"color_1"`    // hex color code
+	Color2     string `json:"color_2"`    // hex color code
+	Color3     string `json:"color_3"`    // hex color code
+	Color4     string `json:"color_4"`    // hex color code
 	// Roster   string `json:"roster"`   // list of PlayerYear objects
 	// Schedule string `json:"schedule"` // list of MatchResult objects
 	// Stats    string `json:"stats"`    // list of TeamMatchStats objects
