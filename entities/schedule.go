eb8e7476d074055cf28b45bcc5e7b3290b64bd9f
Nick <ndloucks@proton.me>
2025-10-30 21:02:21 -0700

    notes for processes to implement later

--- entities/schedule.go
+++ entities/schedule.go
@@ -61,8 +61,6 @@ var StringToMatchName = map[string]MatchName{
 // and the vlaue is an id ref to a MatchResult/Event in that table...
 
 type Schedule struct {
-	Id   string `json:"id"`   // primary key, uuid
-	Team string `json:"team"` // foreign key, subject team for this instance of schedule
 	Year uint16 `json:"year"`
 	// TO-DO: should these lists of MatchResult objects be foreign keys as well to the match results "fact" table?
 	// should they be expanded as named references to specific potential match slots to preserve relational structure?
@@ -75,12 +73,34 @@ type Schedule struct {
 
 func (s *Schedule) SchedulePreSeason() {
 	// TO-DO: instantiates event objects for preseason matches
+	// get list of all 16 teams sorted by end-of-year rankings from previous year
+	// instantiate a 16-team bracket with that list
+	// - make all the MatchEvent objects for the tournament
+	// - 
 }
 
 func (s *Schedule) ScheduleRegSeason() {
 	// TO-DO: instantiates event objects for regular season matches
+	// for each conference:
+	// 		get a list of all 8 TeamYear objects
+	// 		schedule a round-robin tournament with all the TeamYear objects in the conference
+	// 			^ note TO-DO: there must be some mechanism for determining home/away teams w/o using a ranking
+						// - lookup the relevant conf matchup between the teams from prev year, and switch the home/away
+						// - if no conf matchup prev year, then "flip a coin"
+
+	// -------------
+	// figure out how to schedule non-conference games and still include a few cross-conference rivalries every year
 }
 
 func (s *Schedule) SchedulePostSeason() {
 	// TO-DO: instantiates event objects for prostseason matches
+	// only once top two teams in each conference have clinched:
+	// create a single-elim bracket tournament:
+	// ensure that, regardless of actual league ranking, when feeding in ranked list to the bracket constructor,
+	// one conference must put its teams in the 1 & 4 seed slots, while the other puts them in the  2 & 3 slots
+	// so that the correct teams play each other in the "semi-final" round
+	// also need to override the league champ game in that tournament to be neutral site.
+	// --------then:
+	// only after two teams have clinched the next highest spots:
+	// schedule a single MatchEvent for the Pine Bowl
 }
