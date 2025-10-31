409a1d4965222980c4244a8f9147987476c48063
Nick <ndloucks@proton.me>
2025-10-30 20:19:12 -0700

    roster uses teamyear and playeryear

--- entities/roster.go
+++ entities/roster.go
@@ -1,10 +1,6 @@
 package entities
 
 type Roster struct { // not a database table: it's a filtered list of players
-	// Id      string   `json:"id"`   // primary key, uuid
-	Team    string   `json:"team"` // subject team, foreign key, unique
-	Players []Player `json:"players"`
+	TeamYear    TeamYear     `json:"team_year"` // subject team, foreign key, unique
+	PlayerYears []PlayerYear `json:"players"`
 }
-
-// maybe a Roster is really just a "Team" filter applied to a list of PlayerYear objects
-// and therefore does not need an Id or a database table
