d520563948225696e57d7fc7060fc293130f7c43
Nick <ndloucks@proton.me>
2025-10-30 21:01:57 -0700

    keep id at top

--- entities/match_event.go
+++ entities/match_event.go
@@ -4,13 +4,13 @@ package entities
 
 // An event
 type MatchEvent struct {
+	Id          string    `json:"id"`        // primary key
 	AwayRank    uint8     `json:"away_rank"` // away team's rank/seed at beginning of match
 	AwayScore   uint8     `json:"away_score"`
 	AwayTeam    string    `json:"away_team"` // foreign key: Team.name
 	HomeRank    uint8     `json:"home_rank"` // home team's rank/seed at beginning of match
 	HomeScore   uint8     `json:"home_score"`
 	HomeTeam    string    `json:"home_team"`    // foreign key: Team.name
-	Id          string    `json:"id"`           // primary key
 	MatchName   MatchName `json:"match_name"`   // indicates the match's place in the schedule for the year
 	NeutralSite bool      `json:"neutral_site"` // indicates whether the game was played at a neutral site
 	// if the match was played at a neutral site, then the "Home" team is the team with the better rank entering the match
