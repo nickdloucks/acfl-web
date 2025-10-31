aa11f3160412d9b0a7eea9217ac771b93e2b4b42
Nick <ndloucks@proton.me>
2025-10-30 20:18:08 -0700

    add champ game to conference entity

--- entities/conference.go
+++ entities/conference.go
@@ -1,10 +1,11 @@
 package entities
 
 type Conference struct {
-	Id       string `json:"id"`   // primary key, uuid
-	Name     string `json:"name"` // unique
-	Abrv     string `json:"abrv"` // unique, abbreviation for the conference name
-	Color1   string `json:"color_1"`
-	Color2   string `json:"color_2"`
-	LogoPath string `json:"logo_path"`
+	Id               string    `json:"id"`   // primary key, uuid
+	Name             string    `json:"name"` // unique
+	Abrv             string    `json:"abrv"` // unique, abbreviation for the conference name
+	Color1           string    `json:"color_1"`
+	Color2           string    `json:"color_2"`
+	LogoPath         string    `json:"logo_path"`
+	ChampionshipGame MatchName `json:"championship_game"`
 }
