47eee59d40dc8936d1b3194f96f59313bb18af8d
Nick <ndloucks@proton.me>
2025-11-01 11:10:25 -0700

    create player position enumeration and type

--- entities/player_year.go
+++ entities/player_year.go
@@ -1,8 +1,67 @@
 package entities
 
 type PlayerYear struct {
-	Id       string `json:"id"`        // primary key
-	PlayerId string `json:"player_id"` // foreign key to Player
-	Year     uint16 `json:"year"`
-	Team     string `json:"team"` // foreign key to TeamYear
+	Id       string         `json:"id"`        // primary key
+	PlayerId string         `json:"player_id"` // foreign key to Player
+	Year     uint16         `json:"year"`
+	Team     string         `json:"team"` // foreign key to TeamYear
+	Position PlayerPosition `json:"position"`
+}
+
+type PlayerPosition string
+
+const (
+	// --- OFFENSIVE POSITIONS:
+	QB PlayerPosition = "QB"
+	RB PlayerPosition = "RB"
+	FB PlayerPosition = "FB"
+	WR PlayerPosition = "WR"
+	TE PlayerPosition = "TE"
+	LT PlayerPosition = "LT"
+	LG PlayerPosition = "LG"
+	C  PlayerPosition = "C"
+	RG PlayerPosition = "RG"
+	RT PlayerPosition = "RT"
+	// --- DEFENSIVE POSITIONS:
+	DE  PlayerPosition = "DE"
+	DT  PlayerPosition = "DT"
+	OLB PlayerPosition = "OLB"
+	ILB PlayerPosition = "ILB"
+	RCB PlayerPosition = "RCB"
+	LCB PlayerPosition = "LCB"
+	FS  PlayerPosition = "FS"
+	SS  PlayerPosition = "SS"
+	NCB PlayerPosition = "NCB"
+	DCB PlayerPosition = "DCB"
+	// --- SPECIAL TEAMS POSITIONS:
+	K PlayerPosition = "K"
+	P PlayerPosition = "P"
+)
+
+var StringToPosition = map[string]PlayerPosition{
+	// --- OFFENSIVE POSITIONS:
+	"QB": QB,
+	"RB": RB,
+	"FB": FB,
+	"WR": WR,
+	"TE": TE,
+	"LT": LT,
+	"LG": LG,
+	"C":  C,
+	"RG": RG,
+	"RT": RT,
+	// --- DEFENSIVE POSITIONS:
+	"DE":  DE,
+	"DT":  DT,
+	"OLB": OLB,
+	"ILB": ILB,
+	"RCB": RCB,
+	"LCB": LCB,
+	"FS":  FS,
+	"SS":  SS,
+	"NCB": NCB,
+	"DCB": DCB,
+	// --- SPECIAL TEAMS POSITIONS:
+	"K": K,
+	"P": P,
 }
