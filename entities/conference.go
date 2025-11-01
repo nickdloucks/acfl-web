48bae118fd932f0a33951f2bb9965188019e09a2
Nick <ndloucks@proton.me>
2025-11-01 13:45:06 -0700

    add some documenting comments

--- entities/conference.go
+++ entities/conference.go
@@ -5,7 +5,7 @@ type Conference struct {
 	Name             string    `json:"name"` // unique
 	Abrv             string    `json:"abrv"` // unique, abbreviation for the conference name
 	ChampionshipGame MatchName `json:"championship_game"`
-	Color1           string    `json:"color_1"`
-	Color2           string    `json:"color_2"`
-	Color3           string    `json:"color_3"`
+	Color1           string    `json:"color_1"` // hex color code
+	Color2           string    `json:"color_2"` // hex color code
+	Color3           string    `json:"color_3"` // hex color code
 }
--- entities/team.go
+++ entities/team.go
@@ -1,5 +1,7 @@
 package entities
 
+// Used to represent any team that has ever existed in the league.
+// A Team has a one-to-many relationship with TeamYears
 type Team struct {
 	// note: the Team object transcends "Year".
 	// Player stats, team stats, and game results are assigned to a year
