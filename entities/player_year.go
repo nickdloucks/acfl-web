7a6f304fed1bd3b4ef18e6d890dfac061c837a34
Nick <ndloucks@proton.me>
2025-11-01 10:51:09 -0700

    add reff to player id

--- entities/player_year.go
+++ entities/player_year.go
@@ -1,7 +1,8 @@
 package entities
 
 type PlayerYear struct {
-	Id   string `json:"id"` // primary key
-	Year uint16 `json:"year"`
-	Team string `json:"team"` // foreign key
+	Id       string `json:"id"`        // primary key
+	PlayerId string `json:"player_id"` // foreign key to Player
+	Year     uint16 `json:"year"`
+	Team     string `json:"team"` // foreign key to TeamYear
 }
