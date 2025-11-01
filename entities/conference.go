456eafa0e4f821ab773426420e40832266dcf3df
Nick <ndloucks@proton.me>
2025-11-01 12:03:03 -0700

    add 3rd color option, remove logo path which should be handled by presenter or viewer components

--- entities/conference.go
+++ entities/conference.go
@@ -4,8 +4,8 @@ type Conference struct {
 	Id               string    `json:"id"`   // primary key, uuid
 	Name             string    `json:"name"` // unique
 	Abrv             string    `json:"abrv"` // unique, abbreviation for the conference name
+	ChampionshipGame MatchName `json:"championship_game"`
 	Color1           string    `json:"color_1"`
 	Color2           string    `json:"color_2"`
-	LogoPath         string    `json:"logo_path"`
-	ChampionshipGame MatchName `json:"championship_game"`
+	Color3           string    `json:"color_3"`
 }
