6f9fef2e21d0e694175bde354f6943c07efa49c6
Nick <ndloucks@proton.me>
2025-11-02 17:05:02 -0800

    move this out of entities

--- internal/entities/tournament_bracket.go
+++ internal/entities/tournament_bracket.go
@@ -1,13 +1,2 @@
 package entities
 
-
-type SingleElimBracket interface {
-	GetParticipants() []MatchParticipant
-	GetMatches() []MatchEvent
-
-}
-
-// Expects a list of participants that is pre-sorted ascending from "best" to "worst" seed
-func ScheduleBracket(participants []MatchParticipant) SingleElimBracket {
-	NewSingleElimBracket()
-}
