c7dba015c218013c74b39787a77b1cb137216348
Nick <ndloucks@proton.me>
2025-11-02 21:04:44 -0800

    fix copy-pasted test message strings to be more specific

--- internal/entities/conference_test.go
+++ internal/entities/conference_test.go
@@ -20,13 +20,13 @@ func TestValidateConfName(t *testing.T) {
 }
 
 func TestValidateConfAbrv(t *testing.T) {
-	t.Run("returns error with bad input name", func(t *testing.T){
+	t.Run("returns error with bad input abbreviation", func(t *testing.T){
 		got := validateConfAbrv("fake conference abrv")
 		if got == nil {
 			t.Errorf("got %s, want an error", got)
 		}
 	})
-	t.Run("returns no error when given a valid name", func(t *testing.T) {
+	t.Run("returns no error when given a valid abbreviation", func(t *testing.T) {
 		got := validateConfAbrv("MC")
 		if got != nil {
 			t.Errorf("got %s, want nil", got.Error())
@@ -35,13 +35,13 @@ func TestValidateConfAbrv(t *testing.T) {
 }
 
 func TestValidateConfTitleGameStr(t *testing.T) {
-	t.Run("returns error with bad input name", func(t *testing.T){
+	t.Run("returns error with bad input title game", func(t *testing.T){
 		got := validateConfTitleGameStr("fake conference title game")
 		if got == nil {
 			t.Errorf("got %s, want an error", got)
 		}
 	})
-	t.Run("returns no error when given a valid name", func(t *testing.T) {
+	t.Run("returns no error when given a valid title game", func(t *testing.T) {
 		got := validateConfTitleGameStr("Ax Bowl")
 		if got != nil {
 			t.Errorf("got %s, want nil", got.Error())
