d1975fa137a617cd05641ba721260cc36f5022ba
Nick <ndloucks@proton.me>
2025-11-09 07:44:45 -0800

    add more fields to classify the play type

--- internal/entities/play_event.go
+++ internal/entities/play_event.go
@@ -35,34 +35,40 @@ const (
 // able to be easily re-sorted later if necessary.
 
 type PlayBeginState struct {
-	StartTime            TimeRemainingStr `json:"start_time"`
-	TeamInPossession     string           `json:"team_in_possession_start"`
-	StartYdLine          int8             `json:"start_yd_line"`
-	CurrentDown          uint8            `json:"current_down"`
-	CurrentDistance      uint8            `json:"current_distance"`
-	Offense              string           `json:"offense"` // Team.Abrv
-	Defense              string           `json:"defense"` // Team.Abrv
-	OffensePlaySelection string           `json:"off_play_selection"`
-	DefensePlaySelection string           `json:"def_play_selection"`
+	StartTime             TimeRemainingStr `json:"start_time"`
+	TeamInPossessionStart string           `json:"team_in_possession_start"`
+	StartYdLine           int8             `json:"start_yd_line"`
+	CurrentDown           uint8            `json:"current_down"`
+	CurrentDistance       uint8            `json:"current_distance"`
+	Offense               string           `json:"offense"` // Team.Abrv
+	Defense               string           `json:"defense"` // Team.Abrv
+	OffensePlaySelection  string           `json:"off_play_selection"`
+	DefensePlaySelection  string           `json:"def_play_selection"`
 }
 
 type PlayEndState struct {
-	EndTime          TimeRemainingStr `json:"end_time"`
-	TeamInPossession string           `json:"team_in_possession_end"`
-	EndYdLine        int8             `json:"end_yd_line"`
-	NextDown         uint8            `json:"next_down"`
-	NextDistance     uint8            `json:"next_distance"`
-	PlayEndReason    PlayEndReason    `json:"play_end_reason"`
+	EndTime             TimeRemainingStr `json:"end_time"`
+	TeamInPossessionEnd string           `json:"team_in_possession_end"`
+	EndYdLine           int8             `json:"end_yd_line"`
+	NextDown            uint8            `json:"next_down"`
+	NextDistance        uint8            `json:"next_distance"`
+	PlayEndReason       PlayEndReason    `json:"play_end_reason"`
 }
 
 type PlayEvent struct {
-	Id                 UuidV7Str   `json:"id"`
-	Home               string      `json:"home"` // Team.Abrv
-	Away               string      `json:"away"` // Team.Abrv
+	Id                 UuidV7Str   `json:"id"`      // primary key
+	GameId             UuidV7Str   `json:"game_id"` // foreign key
+	Home               string      `json:"home"`    // Team.Abrv foreign key
+	Away               string      `json:"away"`    // Team.Abrv foreign key
 	Quarter            GameQuarter `json:"quarter"`
-	IsSpecialTeamsPlay bool        `json:"is_special_teams_play"`
-	Turnovers          uint8       `json:"turnovers"` // values of 0 or 1 can be parsed as a bool, while higher numbers can still be used in the rare scenario where there are multiple turnovers on the same play
+	IsSpecialTeamsPlay bool        `json:"is_special_teams_play"` // default false
+	Turnovers          uint8       `json:"turnovers"`             // values of 0 or 1 can be parsed as a bool, while higher numbers can still be used in the rare scenario where there are multiple turnovers on the same play
 	PointsScored       uint8       `json:"points_scored"`
 	PlayBeginState
 	PlayEndState
+	RushPlay   bool `json:"rush_play"`
+	PassPlay   bool `json:"pass_play"`
+	PlayAction bool `json:"play_action"`
+	BlitzPlay  bool `json:"blitz_play"`
+	NetYards   int8 `json:"netYards"` // since this is a single play event, this number should fit in int8, but may need conversion for aggragating into game stats
 }
