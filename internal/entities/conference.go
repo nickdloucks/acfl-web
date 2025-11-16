4940cdc033d5faddbbdbfdc058f959f89e7729a4
Nick <ndloucks@proton.me>
2025-11-16 10:50:13 -0800

    don't hardcode conference stuff, leave option to create new conferences later

--- internal/entities/conference.go
+++ internal/entities/conference.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Conference struct {
-	Id               UuidV7Str      `json:"id"`   // primary key, uuid
-	Name             ConferenceName `json:"name"` // unique
-	Abrv             ConferenceAbrv `json:"abrv"` // unique, abbreviation for the conference name
-	ChampionshipGame MatchName      `json:"championship_game"`
+	Id               UuidV7Str `json:"id"`   // primary key, uuid
+	Name             string    `json:"name"` // unique
+	Abrv             string    `json:"abrv"` // unique, abbreviation for the conference name
+	ChampionshipGame MatchName `json:"championship_game"`
 	ConfAdjustableConfig
 }
 
@@ -17,70 +17,6 @@ type ConfAdjustableConfig struct {
 	Color1 ColorStr `json:"color_1"` // hex color code
 	Color2 ColorStr `json:"color_2"` // hex color code
 	Color3 ColorStr `json:"color_3"` // hex color code
-}
-
-// Enumerate allowed Conference fields that will not be editable:
-
-// --- Conference Name ---
-type ConferenceName string
-
-const (
-	MarineConfName ConferenceName = "Marine Conference"
-	SpruceConfName ConferenceName = "Spruce Conference"
-)
-
-// Enumerates allowed conference names.
-var ConfNameStrToConst = map[string]ConferenceName{
-	"Marine Conference": MarineConfName,
-	"Spruce Conference": SpruceConfName,
-}
-
-func validateConfName(name string) error {
-	if _, ok := ConfNameStrToConst[name]; ok {
-		return nil
-	}
-	return errors.New("invalid conference name string")
-}
-
-// --- Conference Abbreviation ---
-type ConferenceAbrv string
-
-const (
-	MarineConfAbrv ConferenceAbrv = "MC"
-	SpruceConfAbrv ConferenceAbrv = "SC"
-)
-
-// Enumerates allowed converence abbreviation strings.
-var ConfAbrvStrToConst = map[string]ConferenceAbrv{
-	"MC": MarineConfAbrv,
-	"SC": SpruceConfAbrv,
-}
-
-func validateConfAbrv(abrv string) error {
-	if _, ok := ConfAbrvStrToConst[abrv]; ok {
-		return nil
-	}
-	return errors.New("invalid conference abbreviation string")
-}
-
-// --- Conference Title Game a.k.a Championship Game ---
-type ConfTitleGame string
-
-const (
-	MCCG ConfTitleGame = "Anchor Bowl"
-	SCCG ConfTitleGame = "Ax Bowl"
-)
-
-var ConfCGStrToConst = map[string]ConfTitleGame{
-	"Anchor Bowl": MCCG,
-	"Ax Bowl":     SCCG,
-}
-
-func validateConfTitleGameStr(ccg string) error {
-	if _, ok := ConfCGStrToConst[ccg]; ok {
-		return nil
-	}
-	return errors.New("invalid conference title game string")
 }
 
 // --- Conference factory func with validation ---
@@ -95,24 +31,16 @@ func NewConference(
 	if id == "" {
 		return nil, errors.New("conference id cannot be empty")
 	}
-	confNameErr := validateConfName(name)
+	newConfName, confNameErr := capitalizeWords(name)
 	if confNameErr != nil {
 		return nil, confNameErr
-	}
-	confAbrvErr := validateConfAbrv(abrv)
-	if confAbrvErr != nil {
-		return nil, confAbrvErr
-	}
-	confTgErr := validateConfTitleGameStr(ccg)
-	if confTgErr != nil {
-		return nil, confTgErr
 	}
 
 	return &Conference{
 		Id:               id,
-		Name:             ConfNameStrToConst[name],
-		Abrv:             ConfAbrvStrToConst[abrv],
-		ChampionshipGame: MatchName(ConfCGStrToConst[ccg]),
+		Name:             newConfName,
+		Abrv:             strings.ToUpper(abrv),
+		ChampionshipGame: MatchName(ccg),
 		ConfAdjustableConfig: ConfAdjustableConfig{
 			Color1: validateColor(string(config.Color1)),
 			Color2: validateColor(string(config.Color2)),
